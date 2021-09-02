3ee1f31058dcb1e37f2ce48bf2e1baf576f5cdb7
Walter <32146758+walt749@users.noreply.github.com>
2021-09-01 22:53:57 -0400

    Update SQL_project.sql

--- SQL/SQL_project.sql
+++ SQL/SQL_project.sql
@@ -1,4 +1,5 @@
 
+--Create a videos table.
 CREate Table videos (
 video_ID INT, 
 title VARCHAR(255) NOT NULL,
@@ -6,12 +7,12 @@ length VARCHAR(255) NOT NULL,
 url VARCHAR(255) NOT NULL);
 
 
-SELECT * FROM videos;
 
+--Create and populate Reviewers table.
 INSERT INTO videos(video_ID,title,length,url) VALUES (1,'The Rules for Rulers','18:12','https://www.youtube.com/watch?v=rStL7niR7gs'),
 INSERT INTO videos(video_ID,title,length,url) VALUES (2,'SQL Tutorial - Full Database Course for Beginners','4:20:38','https://www.youtube.com/watch?v=HXV3zeQKqGY'),
 INSERT INTO videos(video_ID,title,length,url) VALUES (3,'4 steps to master a new technology - Java Brains','15:13','https://www.youtube.com/watch?v=tcW13YfNemw'),
-INSERT INTO videos(video_ID,title,length,url) VALUES (4,'China's Regulatory Crackdown - What You Need to Know Before Investing in Chinese Stocks','14:35','https://www.youtube.com/watch?v=Qw1z9ZJaMFE')
+INSERT INTO videos(video_ID,title,length,url) VALUES (4,'China Regulatory Crackdown - What You Need to Know Before Investing in Chinese Stocks','14:35','https://www.youtube.com/watch?v=Qw1z9ZJaMFE')
 
 
 SELECT * FROM videos;
@@ -33,6 +34,7 @@ INSERT INTO reviews Values(526,'Chase', 3,'was thinking in investing in China',4
 
 SELECT * FROM reviews;
 
+--Report on Video Reviews.
 select * from videos
 full join reviews
 on videos.video_ID = reviews.video_id;
